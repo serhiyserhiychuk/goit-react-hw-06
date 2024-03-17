7b978dadd5029196dc97ab8fcaee8676bd2d9da3
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2024-03-17 11:08:47 -0400

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,10 +7,11 @@ import { selectNameFilter } from "../../redux/filtersSlice";
 export default function ContactList() {
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectNameFilter);
-  const renderContacts = contacts.filter((contact) =>
-    contact.name.includes(
-      filter.charAt(0).toUpperCase() + filter.slice(1).toLowerCase()
-    )
+  const renderContacts = contacts.filter(
+    (contact) =>
+      contact.name.includes(
+        filter.charAt(0).toUpperCase() + filter.slice(1).toLowerCase()
+      ) || contact.name.startsWith(filter.toLowerCase())
   );
   return (
     <ul className={css.list}>
